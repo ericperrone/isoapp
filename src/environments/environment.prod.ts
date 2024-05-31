8b506b3c9951d4e95921a28075f081a7e9059173
ericperrone <87177481+ericperrone@users.noreply.github.com>
2024-05-31 16:01:55 +0200

    Varie

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   be: {
-    protocol: 'http',
-    // protocol: 'https',
-    server: 'localhost:8080',
-    // server: 'itineris-isotope.d4science.org/',
+    // protocol: 'http',
+    protocol: 'https',
+    // server: 'localhost:8080',
+    server: 'itineris-isotope.d4science.org',
     basedir: 'isotopews/rest/'
   }
 };
